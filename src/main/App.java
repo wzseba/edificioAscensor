c687e9ea6b54c1b796cf5e0c6010d5dc32fa76f8
wzseba <wzseba@gmail.com>
2024-08-26 22:15:55 -0300

    add: pruebas para gestionar el ascensor

--- src/main/App.java
+++ src/main/App.java
@@ -4,6 +4,11 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Edificio edificio = new Edificio(10);
+		edificio.gestionarDesplazamiento(10, 3);
+		edificio.gestionarDesplazamiento(10, 4);
+		edificio.gestionarDesplazamiento(10, 10);
+		edificio.gestionarDesplazamiento(10, 4);
 
 	}
 
